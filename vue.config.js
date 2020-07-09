8881b1ac77bc756e1dc483ed1fc15b84c1d8d297
semklauke <privat@semklauke.de>
2020-07-09 15:50:36 +0200

    Project env setup

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+// vue.config.js
+module.exports = {
+    pages: {
+        index: {
+            entry: 'src/main.ts',
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: 'WaS-Tracker'
+        }
+    },
+    
+    outputDir: '../dist',
+    assetsDir: 'assets',
+    indexPath: 'index.html',
+    filenameHashing: true,
+    lintOnSave: false,
+    runtimeCompiler: true,
+    parallel: 4
+}
