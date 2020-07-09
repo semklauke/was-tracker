e0bc7ed87e721ec36d3a07f34fa227a9d89a8c9d
semklauke <privat@semklauke.de>
2020-07-09 20:38:31 +0200

    [FR-MB] Navbar & Station banner desing + qrcode & localStorage libs

--- frontend-mobile/src/App.vue
+++ frontend-mobile/src/App.vue
@@ -1,24 +1,36 @@
 <template>
 <div id="app">
+    <nav class="navbar navbar-light bg-light border-bottom">
+        <span class="navbar-brand mb-0 h1 " id="wastrackerbrand">WaS-Tracker</span>
+    </nav>
+
+    <div class="fixed-bottom bg-primary text-white" id="station_info">
+        <div id="station_info_name">Station 3</div>
+        
+        <div id="station_info_dismiss_span">
+            &#215;
+        </div> 
+        <span class="clearfix"></span>
+    </div>
     <mt-tabbar v-model="activetab">
         <mt-tab-item id="tab-scan">
-            <img slot="icon" class="tabbaricon" src="assets/scan.svg">
+            <img slot="icon" class="tabbaricon" src="assets/scan.svg" />
             SuS
         </mt-tab-item>
         <mt-tab-item id="tab-info">
-            <img slot="icon" class="tabbaricon" src="assets/info.svg">
+            <img slot="icon" class="tabbaricon" src="assets/info.svg" />
             Info
         </mt-tab-item>
         <mt-tab-item id="tab-history">
-            <img slot="icon" class="tabbaricon" src="assets/history.svg">
+            <img slot="icon" class="tabbaricon" src="assets/history.svg" />
             History
         </mt-tab-item>
         <mt-tab-item id="tab-lostqr">
-            <img slot="icon" class="tabbaricon" src="assets/lostqr.svg">
+            <img slot="icon" class="tabbaricon" src="assets/lostqr.svg" />
             Kein QR
         </mt-tab-item>
         <mt-tab-item id="tab-help">
-            <img slot="icon" class="tabbaricon" src="assets/help.svg">
+            <img slot="icon" class="tabbaricon" src="assets/help.svg" />
             Help
         </mt-tab-item>
     </mt-tabbar>
@@ -36,6 +48,46 @@
 .tabbaricon {
     width: 100%;
     height: 100%;
+}
+#station_info {
+    width: 100%;
+    padding: 0px;
+    margin: 0px;
+    min-height: 40px;
+    max-height: 45px;
+    font-size: 1.3em;
+    position: absolute;
+    left: 0px;
+    top: 56px;
+    box-shadow: 0 .125rem .25rem rgba(0,0,0,.425)!important;
+    border-radius: 0px 0px 10px 10px;
+}
+#wastrackerbrand {
+    width: 100%;
+    text-align: center;
+    font-size: 1.4em;
+}
+#station_info_name {
+    width: 60%;
+    display: inline-block;
+    height: 100%;
+    line-height: 45px;
+    padding-left: 15px;
+}
+#station_info_dismiss_span {
+    width: 30%;
+    float: right;
+    font-weight: lighter;
+    color: #BFBFBF;
+    text-align: right;
+    padding-right: 15px;
+    display: inline-block;
+    height: 100%;
+    line-height: 38px;
+    font-size: 2em;
+}
+.filler {
+    width: 100%;
 }
 </style>
 
--- frontend-mobile/src/main.ts
+++ frontend-mobile/src/main.ts
@@ -1,12 +1,10 @@
+// @ts-nocheck
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
 // Mint UI
-
-//@ts-ignore
 import { Tabbar, TabItem } from 'mint-ui';
-
 Vue.component(Tabbar.name, Tabbar);
 Vue.component(TabItem.name, TabItem);
 
@@ -14,18 +12,39 @@ Vue.component(TabItem.name, TabItem);
 
 import axios from 'axios';
 import cookie from 'cookie';
+Vue.prototype.$http = axios;
 
+// vue config
 Vue.config.productionTip = false;
 
-Vue.prototype.$http = axios;
+// adding qrscanner lib
+import jsQR from "jsqr";
+Vue.prototype.$jsqr = jsQR;
+
+// import boostrap for look
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// local stoarge for offline usage
+import VueLocalStorage from 'vue-localstorage';
+Vue.use(VueLocalStorage);
 
 // create vue app
 let vm = new Vue({
     router,
     render: h => h(App),
+    localStorage: {
+        station_name: {
+            type: String,
+            default: null
+        },
+        station_id: {
+            type: String,
+            default: null
+        }
+    }
 }).$mount('#app');
 
-//@ts-ignore
+// axios login interceptor
 vm.$http.interceptors.request.use(function (config: any) {
     config.timeout = 1000;
     config.validateStatus = function (status: number) {
