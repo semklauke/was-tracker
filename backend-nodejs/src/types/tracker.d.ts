e73c25e8a0b20aa9e2c8c8d5e1811b185cff14a9
semklauke <privat@semklauke.de>
2020-07-12 16:30:13 +0200

    [BK-JS] TypeScript Interfaces for db tables

---
+++ backend-nodejs/src/types/tracker.d.ts
@@ -0,0 +1,29 @@
+export interface Station {
+    rec_id?: number,
+    name: string,
+    uuid: string,
+    prev_station?: number,
+    distance_m?: number,
+    position?: string
+}
+
+export interface Code {
+    rec_id?: number,
+    qr: string,
+    active: number
+}
+
+export interface Scanner {
+    rec_id?: number,
+    stamp: string,
+    uuid: string,
+    name?: string
+}
+
+export interface Checkin {
+    rec_id?: number,
+    code_id: number,
+    station_id: number,
+    scanner_id: number,
+    stamp: string
+}
