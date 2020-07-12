2325603ec110254cdcf001d19866913714e9ff48
semklauke <privat@semklauke.de>
2020-07-12 16:29:46 +0200

    [BK-JS] SS Auth helper + logging setup

---
+++ backend-nodejs/src/includes/scanner_authentication.ts
@@ -0,0 +1,35 @@
+/// <reference types="better-sqlite3"/>
+
+import sqlite from 'better-sqlite3';
+import DB from 'better-sqlite3-helper';
+import path from 'path';
+import winston from 'winston';
+import { logger } from './../includes/logger';
+import { User, Login } from './../types';
+import { RequestHandler } from 'express';
+
+export let secure: RequestHandler = function(req, res, next)  {
+    if (req.cookies.scanner_uuid === undefined) {
+        res.status(401).json({ error: "Not logged in", errorid: 3 });
+    } else {
+        let scanner_uuid = req.cookies.scanner_uuid;
+        res.locals.scanner_uuid = req.cookies.scanner_uuid;
+
+        let e: Error | null = null;
+        let u: User | null = null;
+
+        try {
+            let result: Login | undefined = DB().prepare("SELECT * FROM scanner WHERE uuid = ?").get(scanner_uuid); 
+            if (!result) {
+                logger.warn("Unauthorized request from %s", scanner_uuid);
+                res.status(401).json({ error: "Not logged in", errorid: 3 });
+            } else {
+                logger.debug("Authorized %s", result.uuid);
+                next();
+            }
+        } catch (error) {
+            logger.error("DB ERROR | tracker secure");
+        }
+        
+    }
+}
