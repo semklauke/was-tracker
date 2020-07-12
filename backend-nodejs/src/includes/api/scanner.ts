5a013003f4210f914ea61523b3026c4360977734
semklauke <privat@semklauke.de>
2020-07-12 16:30:42 +0200

    [BK-JS] /api/scanner and /api/station/ module

---
+++ backend-nodejs/src/includes/api/scanner.ts
@@ -0,0 +1,81 @@
+// -- scanner api route --
+
+/// <reference types="better-sqlite3"/>
+
+import express from 'express';
+import sqlite from 'better-sqlite3';
+import DB from 'better-sqlite3-helper';
+import { BetterSqlite3Helper } from 'better-sqlite3-helper';
+import bodyParser from 'body-parser';
+import { logger } from './../logger';
+import { v4 as uuidv4 } from 'uuid';
+import { SQL } from './../../types/'
+
+// sql querys
+
+const sql_scanner: SQL = `
+    SELECT rec_id, stamp FROM scanner WHERE uuid = ? AND datetime(scanner.stamp, '+1 day') > datetime('now');
+`;
+
+const sql_station: SQL = `
+    SELECT rec_id, uuid, name FROM stations WHERE uuid = ?;
+`;
+
+// init router
+export const router: express.Router = express.Router();
+
+router.post('/', bodyParser.json(), function(req, res) {
+
+    logger.debug("POST api.tracker.scanner /",);
+
+    if (!req.body.station) {
+        logger.warn("POST api.tracker.scanner / No Station");
+        res.status(400).json({ error: 'add station uuid to request', errorid: 203 });
+        return;
+    }
+
+    // check if station uuid exists
+    let station_uuid: string = req.body.station
+    let check_station = DB().queryFirstCell(sql_station, station_uuid);
+    if (check_station === undefined) {
+        // station uuid not in database
+        logger.warn("POST api.tracker.scanner / Bad station uuid")
+        res.status(404).json({ error: 'station uuid not valid', errorid: 456 });
+        return;
+    }
+
+
+    let scanner_uuid: string = '';
+    if (req.body.scanner) {
+        // request alreay has a scanner uuid
+        let check_scanner = DB().queryFirstCell(sql_scanner, req.body.scanner);
+        if (check_scanner === undefined) {
+            res.status(401).json({ error: "scanner uuid not accepted", errorid: 506 });
+            return;
+        } else {
+            scanner_uuid = req.body.scanner;  
+        }
+    } else {
+        // create new scanner uuid cause station id is valid
+        scanner_uuid = uuidv4();
+        let obj: BetterSqlite3Helper.DataObject = {
+            uuid: scanner_uuid
+        };
+        if (req.body.scanner_name) obj['name'] = req.body.scanner_name;
+        try {
+            DB().insert('scanner', obj);
+        } catch (e) {
+            logger.error("DB ERROR api.tracker.scanner / Insert new scanner uuid");
+            res.status(500).json({ error: "Server Error", errorid: 666 });
+            return;
+        }
+    }
+
+    logger.info(
+        "Scanner %s registerd at station %s",
+        scanner_uuid,
+        check_station.name
+    );   
+
+    res.status(200).json({ success: "Scanner registred", scanner_uuid, station_name: check_station.name });
+});
---
+++ backend-nodejs/src/includes/api/station.ts
@@ -0,0 +1,72 @@
+// -- station api route --
+
+/// <reference types="better-sqlite3"/>
+
+import express from 'express';
+import sqlite from 'better-sqlite3';
+import DB from 'better-sqlite3-helper';
+import { BetterSqlite3Helper } from 'better-sqlite3-helper';
+import bodyParser from 'body-parser';
+import { logger } from './../logger';
+import { SQL } from './../../types/';
+import { Station } from './../../types/tracker';
+import { secure } from './../scanner_authentication';
+
+// sql querys
+
+const sql_station_all: SQL = `
+    SELECT * FROM stations;
+`;
+
+const sql_station: SQL = `
+    SELECT * FROM stations WHERE uuid = ?;
+`;
+
+// init router
+export const router: express.Router = express.Router();
+
+/*router.post('/', bodyParser.json(), function(req, res) {
+
+    logger.debug("POST api.tracker.station /",);
+
+});*/
+
+router.get('/', secure,  function(req, res) {
+
+    logger.debug("GET api.tracker.station /",);
+    let stations: any[] = DB().query(sql_station_all);
+    res.status(200).json({ success: "Success", stations, length: stations.length });
+
+});
+
+router.get('/:station_uuid', secure, function(req, res) {
+
+    let station_uuid: string = req.params.station_uuid;
+    logger.debug("GET api.tracker.station /:station_uuid");
+
+    // check if station uuid exists
+    let station: Station | undefined = DB().queryFirstRow(sql_station, station_uuid);
+    if (station == undefined) {
+        logger.debug("GET api.tracker.station /%s (:station__ud) No such station", station_uuid);
+        res.status(404).json({ error: 'The station does not exists', errorid: 210 });
+        return;
+    }
+
+    res.status(200).json({ success: 'Success', station });
+});
+
+/*
+router.put('/:station_uuid', bodyParser.json(), function(req, res) {
+
+    let station_uuid: string = req.params.station_uuid;
+    logger.debug("PUT api.tracker.station /:station_uuid");
+
+});
+
+router.delete('/:station_uuid', bodyParser.json(), function(req, res) {
+
+    let station_uuid: string = req.params.station_uuid;
+    logger.debug("DELETE api.tracker.station /:station_uuid");
+
+});
+*/
