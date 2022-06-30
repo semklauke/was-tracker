8a29680baabe506c5d5164c1f576fb8cbf6de19c
semklauke <privat@semklauke.de>
2022-06-30 04:40:36 +0200

    Converted Scan view to Ionic and composite api vue 3

--- frontend-mobile/src/views/Scan.vue
+++ frontend-mobile/src/views/Scan.vue
@@ -1,274 +1,311 @@
-<template>
-    <div id="tab-scan-container" class="container-xl">
-        <div class="row">
-            <div class="col-12" id="scan-heading">
-                <h2>
-                    SuS scannen
-                    <span class="scan-cancel-button" v-if="animate">
-                        <button type="button" class="btn btn-dark" @click="closeCamera">
-                            &#215;
-                        </button>
-                    </span>
-                </h2>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12" >
-                <div id="scan-scan-container" :class="{ scanBackground: !animate }" @click="openCamera">
-                    <canvas id="scan-scan-canvas" hidden></canvas>
-                </div>
-            </div>
-        </div>
-        <div class="row scan-info" v-if="request_info">
-            <div class="col">
-                <div class="alert " :class="request_info_classes" role="alert">
-                    {{ request_info_text }}
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
+<script lang="ts" setup>
+import { ref, inject, onMounted } from 'vue';
+import type { Ref, ComponentPublicInstance } from 'vue';
+import { onIonViewWillEnter, onIonViewWillLeave } from '@ionic/vue';
+import QrScanner from 'qr-scanner';
+import type { AxiosInstance } from 'axios';
+import { useOfflineStore } from '@/stores/offlineStore';
+import type { ScanCode } from '@/stores/offlineStore';
+import { dateToSqliteTimestamp, was_alert } from '@/includes/helper';
+// import components
+import {
+    IonPage,
+    IonContent,
+    IonGrid,
+    IonCol,
+    IonRow,
+    IonButton,
+    IonIcon,
+    IonText
+} from '@ionic/vue';
+// import icons
+import { 
+    camera,
+    cameraOutline,
+    flashlightOutline
+} from 'ionicons/icons';
 
-<script>
-/*UPGRADE*/
-//import { Toast } from 'mint-ui';
-function Toast({ message, duration }) {}
+type Timer = number
 
-export default {
-    data: function () {
-        return {
-            animate: false,
-            stop_cam: false,
-            request_info: false,
-            request_info_classes: {
-                'alert-success': false,
-                'alert-danger': false,
-                'alert-warning': false
-            },
-            request_info_text: ''
-        };
-    },
-    _canvas: null, 
-    _stream: null,
-    _lastcode: null,
-    _codecount: null,
-    _rAF: null,
-    components: {
-    },
-    mounted() {
-        if (navigator === undefined 
-         || navigator.mediaDevices === undefined 
-         || navigator.mediaDevices.getUserMedia === undefined) {
-            alert("Ihr Brwoser unterstützt keine Cameras bzw. hat nicht die Berechtigung diese zu nutzen");
-            return;
-        }
-    },
-    methods: {
-        drawQRInfo(location) {
-            let canvas = this.$options._canvas;
-            canvas.lineWidth = 5;
-            canvas.strokeStyle = "#FF2D2D";
-            canvas.beginPath();
-            canvas.moveTo(location.topLeftCorner.x, location.topLeftCorner.y);
-            let f = corner => {
-                canvas.lineTo(corner.x, corner.y);
-                canvas.moveTo(corner.x, corner.y);
-            }
-            f(location.topRightCorner);
-            f(location.bottomRightCorner);
-            f(location.bottomLeftCorner);
-            f(location.topLeftCorner);
-            canvas.stroke();
-        },
-        openCamera() {
-            if (this.animate == true)
-                return;
+/* --- composable and inject --- */
+const $http: AxiosInstance = inject('$http') as AxiosInstance
+let qrScanner: QrScanner | null;
+const offline_store = useOfflineStore();
 
-            let video = document.createElement("video");
-            let canvasElement = document.getElementById("scan-scan-canvas");
-            let canvas = canvasElement.getContext("2d");
-            this.$options._canvas = canvas;
+/* --- data --- */
+let showVideo = ref(true)
+let hasFlash = ref(false)
+let loading = ref(false)
+let showInfotext = ref(false)
+let cameras = ref<QrScanner.Camera[]>([])
+let infotext_color = ref("success")
+let toggleCamButton = ref() as Ref<ComponentPublicInstance<typeof IonButton>>
+let infotext = ref("")
+let infotext_timer: Timer | undefined;
 
-            let fps_then;
-            let fps_start;
-    
-            let tick = () => {
-                if (this.stop_cam) {
-                    this.stop_cam = false;
-                    cancelAnimationFrame(this.$options._rAF);
-                    return;
-                }
+/* --- lifecycle hooks --- */
+onMounted(() => {
+    showVideo.value = false;
+    QrScanner.hasCamera().then(async () => {
+        cameras.value = await QrScanner.listCameras()
+    }).catch(() => {
+        alert("Ihr Brwoser unterstützt keine Cameras bzw. hat nicht die Berechtigung diese zu nutzen");
+    });
+})
 
-                this.$options._rAF = requestAnimationFrame(tick);
+onIonViewWillEnter(async () => {
+    startCam();
+    displayInfotext("✓ QR-Code gesendet!", "success")
+})
 
-                let fps_now = Date.now().valueOf();
-                let fps_elapsed = fps_now - fps_then;
-                
-                if (fps_elapsed > 120) {
-                    fps_then = fps_now - (fps_elapsed % 120);
+onIonViewWillLeave(() => {
+    stopCam();
+})
 
-                    if (video.readyState == video.HAVE_ENOUGH_DATA) {
-                        canvas.clearRect(0,0,canvasElement.width,canvasElement.height);
-                        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-                        let imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-                        let code = this.$jsqr(imageData.data, imageData.width, imageData.height, {
-                            inversionAttempts: "dontInvert",
-                        });
 
-                        if (code) {
-                            if (this.$options._lastcode === null) {
-                                this.$options._lastcode = code.data;
-                            } else if (code.data == this.$options._lastcode) {
-                                saveCode(code.data);
-                            }
-                            
-                        }
-                    }
-                }
-                         
-            }
-            navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(stream => {
-                this.animate = true;
-                this.stop_cam = false;
-                this.$options._stream = stream;
-                video.srcObject = stream;
-                video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-                video.play();
-                let tickSetup = () => {
-                    if (video.readyState == video.HAVE_ENOUGH_DATA) {
-                        canvasElement.hidden = false;
-                        canvasElement.height = video.videoHeight;
-                        canvasElement.width = video.videoWidth;
-                        fps_then = Date.now();
-                        fps_start = fps_then;
-                        this.$options._rAF = requestAnimationFrame(tick);
-                    } else {
-                        this.$options._rAF = requestAnimationFrame(tickSetup);
-                    }
-                } 
-                this.$options._rAF = requestAnimationFrame(tickSetup);
-            });
-        },
-        saveCode(code) {
-            this.animate = false;
-            this.stop_cam = true;
-            cancelAnimationFrame(this.$options._rAF);
-            this.drawQRInfo(code.location);
-            this.$http.post(
-                "/api/checkin/station/" + this.$localStorage.get('station_id', 'error'), 
-                { code_uuid: code }
-            ).then(res => {
-                if (res.status == 404) {
-                    this.saveCodeOffline(code);
-                    this.request_info_text = "Läufer offline gespeichert!";
-                    this.toggleRequestClass('alert-warning');
-                    /*UPGRADE*/
-                    Toast({
-                        message: '✓',
-                        duration: 3500
-                    });            
-                } else if (res.status != 200) {
-                    this.request_info_text = "Fehler aufgetreten!";
-                    this.toggleRequestClass('alert-danger');
-                    /*UPGRADE*/
-                    Toast({
-                        message: '✗',
-                        duration: 3500,
-                    }); 
-                } else {
-                    // worked 
-                    this.request_info_text = "Läufer regrestriert!";
-                    this.toggleRequestClass('alert-success');
-                    /*UPGRADE*/
-                    Toast({
-                        message: '✓',
-                        duration: 3500,
-                    });
-                }
-                this.request_info = true;
-                this.closeCamera();
-                this.resetRequestInfo();
-            }).catch(e => {
-                alert(e.toString());
-                this.request_info_text = "Fehler aufgetreten!";
-                this.toggleRequestClass('alert-danger');
-                this.request_info = true;
-                this.closeCamera();
-                this.resetRequestInfo();
-            });
-        },
-        saveCodeOffline(code) {
-            /*UPGRADE*/
-            let keys = this.$localStorage.get('offline_codes', '');
-            this.$localStorage.set('offline_codes', keys+code+";");
-            let value = Math.floor(Date.now()/1000).toString() + ";";
-            value += this.$localStorage.get('station_id', 'null');
-            localStorage.setItem(code, value);
-        },
-        closeCamera() {
-           cancelAnimationFrame(this.$options._rAF);
-           if (this.$options._stream != null) {
-               this.$options._stream.getTracks().forEach(track => { track.stop(); });
-               this.$options._stream = null;
-           }
-           this.animate = false;
-           this.stop_cam = true;
-           let canvasElement = document.getElementById("scan-scan-canvas");
-           let canvas = canvasElement.getContext("2d");
-           canvas.clearRect(0,0,canvasElement.width,canvasElement.height);
-        },
-        toggleRequestClass(c) {
-            this.request_info_classes['alert-success'] = false;
-            this.request_info_classes['alert-warning'] = false;
-            this.request_info_classes['alert-danger'] = false;
-            if (c) this.request_info_classes[c] = true;
-        },
-        resetRequestInfo() {
-            setTimeout(() => {
-                this.request_info = false;
-                this.toggleRequestClass();
-                this.request_info_text = '';
-            }, 3500);
-        }
-    },
-    beforeRouteLeave(to, from, next) {
-        this.closeCamera();
-        next();
+/* --- methods --- */
+
+async function scannedCode(result: QrScanner.ScanResult) {
+    qrScanner?.pause();
+    // todo: add waiting animation here
+    loading.value = true;
+
+    // parse qr
+    let qrData = result.data.split(";");
+    if (qrData.length != 2 || qrData[0].toLowerCase() != 'walker') {
+        was_alert("QR Code enthält feherlhafte Informationen", "Info");
+        qrScanner?.start();
+        return;
     }
-};
+
+    try {
+    if (!navigator.onLine) {
+        loading.value = false;
+        await saveCodeOffline(qrData[1])
+    } else {
+        let station = offline_store.station_uuid || "error"
+        let res = await $http.post(`/api/checkin/station/${station}`, );
+        loading.value = false;
+
+        if (res.status != 200 && 
+            res?.data?.errorid && 
+            res?.data?.error) {
+            // handle error
+            console.error(res?.data?.error)
+            switch (res?.data?.errorid) {
+                case 287:
+                    was_alert("Station nicht bekannt", "Fehler")
+                    break;
+                case 4058:
+                    was_alert("QR-Code/Läufer*in nicht bekannt.", "Fehler")
+                    break;
+                default:
+                    was_alert("Fehler aufgetreten. Bitte erneut probieren.", "Fehler")
+                    break;
+            }
+            displayInfotext("✗ Nicht gespeichert", "danger")
+        } else if (res?.data?.success) {
+            // request success
+            displayInfotext("✓ QR-Code gesendet!", "success")
+        }
+    } } catch (err: any) {
+        if (!navigator.onLine && err?.code === "ERR_NETWORK") {
+            was_alert("Keine Internet Verbindung", "Info")
+        } else {
+            if (!err) err = new Error("API request error")
+            console.error(err)
+        }
+        displayInfotext("✗ Nicht gespeichert", "danger")
+    } finally {
+        qrScanner?.start();
+        loading.value = false;
+    }
+}
+
+async function saveCodeOffline(uuid: string) {
+    let timestamp = dateToSqliteTimestamp(new Date());
+    let station_uuid = offline_store.station_uuid || "error"
+    let newCode: ScanCode = {
+        uuid,
+        timestamp,
+        station_uuid
+    }
+    offline_store.offline_codes.push(newCode)
+    displayInfotext("(✓) Offline Gespeichert", "warning")
+}
+
+function toggleCam() {
+    if (showVideo.value)
+        stopCam();
+    else
+        startCam();
+}
+
+function startCam() {
+    if (!qrScanner) initCam();
+    qrScanner?.start().then(async () => {
+        showVideo.value = true;
+        hasFlash.value = <boolean> await qrScanner?.hasFlash();
+    }).catch((e: any) => {
+        if (!e) e = new Error("QR-Scanner konnte nicht starten")
+        console.error(e)
+    })
+}
+
+function stopCam() {
+    qrScanner?.stop();
+    showVideo.value = false;
+}
+
+function initCam(force = false) {
+    if (qrScanner && force) {
+        qrScanner?.destroy();
+        qrScanner = null;
+    } else if (!qrScanner) {
+        let videoElem = document.getElementById("scan_video") as HTMLVideoElement
+        qrScanner = new QrScanner(videoElem, scannedCode, {
+            highlightScanRegion: true,
+            highlightCodeOutline: true,
+            returnDetailedScanResult: true,
+            onDecodeError: () => {}
+        });
+    }
+}
+
+
+async function changeCam(cam_id: QrScanner.DeviceId) : Promise<void> {
+    if (toggleCamButton.value) 
+        toggleCamButton.value.$el.disabled = true;
+    await qrScanner?.setCamera(cam_id)
+    cameras.value = await QrScanner.listCameras();
+    if (toggleCamButton.value) 
+        toggleCamButton.value.$el.disabled = false;
+}
+
+async function displayInfotext(text: string, color = "") {
+    showInfotext.value = true;
+    infotext.value = text;
+    infotext_color.value = color;
+    clearTimeout(infotext_timer);
+    infotext_timer = setTimeout(() => {
+        showInfotext.value = false;
+    }, 3000)
+}
+
 </script>
 
-<style>
-#scan-heading {
+<template>
+    <ion-page>
+        <ion-content fullscreen scroll-y="false">
+            <ion-grid>
+                <ion-row class="scan_header_row">
+                    <ion-col>
+                        <h4 class="ion-text-center">QR-Code Scannen</h4>
+                    </ion-col>
+                </ion-row>
+                <ion-row class="scan_header_row">
+                    <ion-col>
+                        <ion-button expand="block" color="primary" @click="toggleCam" ref="toggleCamButton" id="toggleCamButton">
+                            <ion-icon slot="start" :icon="cameraOutline"></ion-icon>
+                            Kamera {{ showVideo ? "Aus" : "An" }}
+                        </ion-button>
+                    </ion-col>
+                </ion-row>
+                <ion-row>
+                    <ion-col>
+                        <div class="ion-justify-content-center ion-align-items-center" 
+                             id="scan_container"
+                        >
+                            <ion-icon :icon="camera" id="scan_icon" v-show="!showVideo" />
+                            <video 
+                                id="scan_video"
+                                disablepictureinpicture
+                                muted
+                                playsinline
+                            ></video>
+                        </div>
+                    </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-around cam_button_row">
+                    <ion-col v-if="hasFlash">
+                        <ion-button  @click="qrScanner?.toggleFlash" color="light">
+                            <ion-icon slot="icon-only" :icon="flashlightOutline"></ion-icon>
+                        </ion-button>
+                    </ion-col>
+                    <ion-col v-for="(cam, index) in cameras" >
+                        <ion-button @click="changeCam(cam.id)" color="medium">
+                            <ion-icon slot="start" :icon="cameraOutline"></ion-icon>
+                            #{{index+1}}
+                        </ion-button>
+                    </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-center ion-text-center infotext">
+                    <ion-col>
+                        <Transition name="fade">
+                        <ion-text :color="infotext_color" v-show="showInfotext">
+                            {{ infotext }}
+                        </ion-text>
+                        </Transition>
+                    </ion-col>
+                </ion-row>
+            </ion-grid>
+        </ion-content>
+    </ion-page>
+</template>
+
+<style scoped>
+#scan_icon {
+    display: inline;
+}
+#scan_container {
+    max-height: 40vh;
+    padding-top: 5px;
+    padding-bottom: 5px;
+}
+#scan_video {
     width: 100%;
-    text-align: center;
-}
-.scanBackground {
-    background-color: rgba(0,0,0,0.3);
-    background-image: url('/assets/scan.svg');
-}
-#scan-scan-container {
-    max-height: 800px;
-    min-height: 350px;
-    background-size: 50%;
-    background-repeat: no-repeat;
-    background-position: center; 
-    z-index: 10;
-}
-#scan-scan-canvas {
-    width: 100%;
-    max-height: 800px;
     max-width: 100%;
-    z-index: 100;
 }
-.scan-cancel-button button {
-    min-width: 40px;
-    float: right;
+
+.scan_header_row h4 {
+    margin-top: 8px;
+    margin-bottom: 6px;
 }
-.scan-info {
-    margin-top: 15px;
+.cam_button_row ion-col {
+    padding-top: 0px;
+    padding-bottom: 0px;
 }
-.mint-toast-text {
-    font-size: 3em;
+
+.cam_button_row {
+    padding: 2px 0px;
+} 
+.cam_button_row ion-button,
+.cam_button_row button {
+    width: 100%;
+}
+#toggleCamButton {
+    /*width: calc(100% - 32px);
+    margin: 5px 16px 5px 16px;*/
+    width: 100%;
+}
+.infotext {
+    font-size: 1.2em;
+    padding: 10px 0px;
+}
+.invisible {
+    visibility: hidden;
+}
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 1s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.fade-leave-from,
+.fade-enter-to {
+    opacity: 1;
 }
 </style>
